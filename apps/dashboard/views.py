6e1d76a883e5bc83f4653473262ca0ad7c0f309c
carvalhocaio <caiocarvalho.py@gmail.com>
2023-02-16 01:44:15 -0400

    Criando view para visitantes no mes atual

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 from visitantes.models import Visitante
 
 
@@ -9,11 +10,16 @@ def index(request):
     visitantes_em_visita = todos_visitantes.filter(status="EM_VISITA")
     visitantes_finalizado = todos_visitantes.filter(status="FINALIZADO")
 
+    hora_atual = timezone.now()
+    mes_atual = hora_atual.month
+    visitantes_mes = todos_visitantes.filter(horario_chegada__month=mes_atual)
+
     context = {
         "nome_pagina": "Início da Dashboard",
         "todos_visitantes": todos_visitantes,
         "visitantes_aguardando": visitantes_aguardando.count(),
         "visitantes_em_visita": visitantes_em_visita.count(),
         "visitantes_finalizados": visitantes_finalizado.count(),
+        "visitantes_mes": visitantes_mes.count(),
     }
     return render(request, "index.html", context)
