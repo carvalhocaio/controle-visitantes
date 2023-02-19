1d746a48e9276b9b84deedbf7066d716f9084ae3
carvalhocaio <caiocarvalho.py@gmail.com>
2023-02-19 13:05:00 -0400

    Criando URL para logout

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -15,6 +15,11 @@ urlpatterns = [
     path(
         "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
     ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+        name="logout",
+    ),
     path("", index, name="index"),
     path("registrar-visitante/", registrar_visitante, name="registrar_visitante"),
     path("visitantes/<int:id>/", informacoes_visitante, name="informacoes_visitante"),
